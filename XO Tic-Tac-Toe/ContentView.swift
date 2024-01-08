67a8651230b590340daac9b1a2da0adb9a9eb046
BJTSoftware <Ben@BenTempleton.com>
2024-01-08 09:47:31 -0800

    Initial Commit

---
+++ XO Tic-Tac-Toe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XO Tic-Tac-Toe
+//
+//  Created by Benjamin Templeton on 1/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XO Tic-Tac-Toe/XO_Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  XO_Tic_Tac_ToeApp.swift
+//  XO Tic-Tac-Toe
+//
+//  Created by Benjamin Templeton on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct XO_Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
